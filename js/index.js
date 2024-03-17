a7f6973ae7d01b5e130c8b05430b3492d8ee06fb
Daur <ndauir02@gmail.com>
2024-03-17 17:04:32 +0600

    homework_9

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// document.body.style.background = 'red';
+
+// if (confirm("Перейти на Wikipedia?")) {
+//     location.href = "https://wikipedia.org"; // Перенаправляет браузер на другой URL
+// }
+  
